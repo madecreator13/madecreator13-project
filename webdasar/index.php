466c3e45895aca44dc281c741b45d618c97c71b7
madecreator13 <khemalezidane@gmail.com>
2023-04-30 20:21:35 +0700

    menambahkan file index.php

--- webdasar/index.php
+++ webdasar/index.php
@@ -1,7 +1,16 @@
 <?php
+$nama_kamu="Khemal";
+$umur=23;
 //tanda petik dua
-echo "hello world";
+/*
+echo "Hello World, Welcome ".$nama_kamu;
+echo " Umur kamu ".$umur;
+echo " tahun";
 echo "<br/>";
-//tanda petik satu
-echo 'hello world';
+*/
+echo "Hello World, Welcome ".$nama_kamu." umur kamu ".$umur." tahun";
+echo "<br/>";
+echo "Berarti $nama_kamu lahir di tahun ";
+echo 2023-$umur;
+
 ?>
